9a1a6a0b14b3a44f3d83f0d2dc60f6f05fbb2786
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-05-27 17:28:07 +0300

    AddActorAndProducer

--- src/components/Admin/Films/Film.js
+++ src/components/Admin/Films/Film.js
@@ -1,74 +1,170 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { escapeLeadingUnderscores } from "typescript";
+import "../../../assets/css/style.css";
 
-export default function Film({ film, onUpdate, onDelete, genres }) {
+export default function Film({ film, onUpdate, onDelete, genres, actors, producers, companies }) {
 
-    const [currentFilm, setcurrentFilm] = useState({...film});
+    //состояния
+    const [currentFilm, setcurrentFilm] = useState({ ...film });
+    //метод, который вызывается первый при загрузки страницы, здесь заполняем страницу детальными данными о фильме
     useEffect(() => {
         setcurrentFilm(film);
     }, [film])
-    function handleSubmit(e) {
+    //медот отвечает за обработку клика кнопки обновить
+    function handleUpdate(e) {
         e.preventDefault();
-        // console.log(newfilm);
         onUpdate(currentFilm);
     };
+    //метод отвечает за обработку удаления
     function handleDelete(e) {
         e.preventDefault();
         onDelete(film);
     };
+    //метод отвечает за обработку изменения полля ввода (всех)
     function handleInput(key, e) {
+        //в переменную создали копию переменной выбранного фильма
         var state = Object.assign({}, currentFilm);
+        //
         state[key] = e.target.value;
         setcurrentFilm(state);
-        console.log(currentFilm);
     };
+    //метод отвечает за обработку изменения жанров выбранного фильма
     function handleInputGenre(e) {
-      //  e.preventDefault();
+
+        //в переменную создали копию переменной выбранного фильма
         var state = Object.assign({}, currentFilm);
+        //хранит код жанра
         var value = Number(e.target.value)
-        var genre={};
-            genre.film_id=Number(film.id);
-            genre.genre_id=value;
-        var key= 'genrefilms';
-            if(state[key].filter(g => g.genre_id=== value).length === 0)
+        //переменная объекта жанра
+        var genre = {};
+        //присваиваем обьекту жанра код фильма (используя пропс)
+        genre.film_id = Number(film.id);
+        ///присваиваем обьекту жанра код жанра (используя переменную)
+        genre.genre_id = value;
+        var key = 'genrefilms';
+        //проверка на наличие в жанрах фильма ключа который соответствует checkbox
+        if (state[key].filter(g => g.genre_id === value).length === 0)
+            //добавляем обьект жанра
             state[key].push(genre);
-            else 
-            state[key].splice(state[key].map(function(g) { return g.genre_id; }).indexOf(value), 1);
-       // console.log(genrefilms);
-       // setCart(cart => [...cart, film]);
-      setcurrentFilm(state);
-        
-        console.log(state['genrefilms']);
+        else
+            //иначе вырезаем обьект из масива жанров фильма
+            state[key].splice(state[key].map(function (g) { return g.genre_id; }).indexOf(value), 1);
+        //записаем копию в оригинал детальный фильм
+        setcurrentFilm(state);
     }
+    function handleInputActor(e) {
+        //в переменную создали копию переменной выбранного фильма
+        var state = Object.assign({}, currentFilm);
+        //хранит код жанра
+        var value = Number(e.target.value)
+        //переменная объекта жанра
+        var actor = {};
+        //присваиваем обьекту жанра код фильма (используя пропс)
+        actor.film_id = Number(film.id);
+        ///присваиваем обьекту жанра код жанра (используя переменную)
+        actor.actor_id = value;
+        var key = 'actorfilms';
+        //проверка на наличие в жанрах фильма ключа который соответствует checkbox
+        if (state[key].filter(g => g.actor_id === value).length === 0)
+            //добавляем обьект жанра
+            state[key].push(actor);
+        else
+            //иначе вырезаем обьект из масива жанров фильма
+            state[key].splice(state[key].map(function (g) { return g.actor_id; }).indexOf(value), 1);
+        //записаем копию в оригинал детальный фильм
+        setcurrentFilm(state);
+    }
+    function handleInputProducer(e) {
+        //в переменную создали копию переменной выбранного фильма
+        var state = Object.assign({}, currentFilm);
+        //хранит код жанра
+        var value = Number(e.target.value)
+        //переменная объекта жанра
+        var producer = {};
+        //присваиваем обьекту жанра код фильма (используя пропс)
+        producer.film_id = Number(film.id);
+        ///присваиваем обьекту жанра код жанра (используя переменную)
+        producer.producer_id = value;
+        var key = 'producerfilms';
+        //проверка на наличие в жанрах фильма ключа который соответствует checkbox
+        if (state[key].filter(g => g.producer_id === value).length === 0)
+            //добавляем обьект жанра
+            state[key].push(producer);
+        else
+            //иначе вырезаем обьект из масива жанров фильма
+            state[key].splice(state[key].map(function (g) { return g.producer_id; }).indexOf(value), 1);
+        //записаем копию в оригинал детальный фильм
+        setcurrentFilm(state);
+    }
+    //рендер если фильм отсутствует
     if (!film) {
         return (<div>  Film Doesnt exist </div>);
     }
     else {
-        return currentFilm ? 
-         (
-            <div >
+        return currentFilm ?
+            (
+                <div >
                     <label> Name:
                         <input type="text" value={currentFilm.name} onChange={(e) => handleInput('name', e)} />
                     </label>
                     <label> Description:
-                    <input type="text" value={currentFilm.description!=null?currentFilm.description:""} onChange={(e) => handleInput('description', e)} />
+                    <input type="text" value={currentFilm.description != null ? currentFilm.description : ""} onChange={(e) => handleInput('description', e)} />
                     </label>
-                    <input type="hidden" value={currentFilm.id}  />
-                        {(genres.map((genre,i) => {
-                            return (
-                                <div key={i}> 
-                                <input  onChange={(e) => handleInputGenre(e)} 
-                                checked={currentFilm.genrefilms?.filter(g => g.genre_id === genre.id).length > 0?"checked":""} type="checkbox" 
-                                value={genre.id} /> 
+                    <input type="hidden" value={currentFilm.id} />
+                    {
+                        //вывод списка жанров в виде checkbox
+                    }
+                    {(genres.map((genre, i) => {
+                        return (
+                            <div key={i}>
+                                <input onChange={(e) => handleInputGenre(e)}
+                                    checked={currentFilm.genrefilms?.filter(g => g.genre_id === genre.id).length > 0 ? "checked" : ""} type="checkbox"
+                                    value={genre.id} />
                                 {genre.name}
-                                </div>
+                            </div>
+                        );
+                    }))}
+                    {(actors.map((actor, i) => {
+                        return (
+                            <div key={i}>
+                                <input onChange={(e) => handleInputActor(e)}
+                                    checked={currentFilm.actorfilms?.filter(g => g.actor_id === actor.id).length > 0 ? "checked" : ""} type="checkbox"
+                                    value={actor.id} />
+                                {actor.name}
+                            </div>
+                        );
+                    }))}
+                    {(producers.map((producer, i) => {
+                        return (
+                            <div key={i}>
+                                <input onChange={(e) => handleInputProducer(e)}
+                                    checked={currentFilm.producerfilms?.filter(g => g.producer_id === producer.id).length > 0 ? "checked" : ""} type="checkbox"
+                                    value={producer.id} />
+                                {producer.name}
+                            </div>
+                        );
+                    }))}
+                    <select onChange={(e) => handleInput('company_id', e)} value={currentFilm.company_id !== null ? currentFilm.company_id : ""}>
+                        {(companies.map((company, i) => {
+
+                            return (
+                                <option key={i} value={company.id}  >{company.name}</option>
                             );
                         }))}
-                    <button onClick={(e) => handleSubmit(e)}>Update</button>
+                    </select>
+                    <form >
+                            <input type="file" name="image"  placeholder="Select file..."/>
+                            <br />
+                            <br />
+                            <button type="submit" name="upload">
+                                Upload
+                            </button>
+                        </form>
+                    <button onClick={(e) => handleUpdate(e)}>Update</button>
                     <button onClick={(e) => handleDelete(e)}>Delete</button>
-            </div>
-         )
-         : (<div key={film.name}><p>Loading...</p></div>);
+                </div>
+            )
+            : (<div key={film.name}><p>Loading...</p></div>);
     }
 };
--- src/components/Admin/Films/Films.js
+++ src/components/Admin/Films/Films.js
@@ -6,6 +6,10 @@ export default function Films() {
     const [allfilms, setallfilms] = useState([]);
     const [currentFilm, setcurrentFilm] = useState(null);
     const [genres, setGenres] = useState();
+    const [allactors, setActors] = useState();
+    const [allproducers, setProducers] = useState();
+    const [allcompanies, setCompanies] = useState();
+    //метод, который заполняет данными переменную выбранного фильма (перересовка выбраного фильма)
     function getFilmDetail(film){
         axios
         .get("http://localhost:8000/api/admin-films/"+film.id)
@@ -15,12 +19,12 @@ export default function Films() {
             },
         );
     }
+    //метод, который вызывается первый при загрузки страницы, здесь заполняем фильмами страницу, а также список жанров
     useEffect(() => {
         axios
             .get("http://localhost:8000/api/admin-films")
             .then(
                 (response) => {
-                  //  console.log(response);
                     setallfilms(response.data);
                 },
             );
@@ -28,33 +32,52 @@ export default function Films() {
             .get("http://localhost:8000/api/admin-genres")
             .then(
                 (response) => {
-                    //  console.log(response);
                     setGenres(response.data);
                 },
             );
+            axios
+            .get("http://localhost:8000/api/admin-actors")
+            .then(
+                (response) => {
+                    setActors(response.data);
+                },
+            );
+            axios
+            .get("http://localhost:8000/api/admin-producers")
+            .then(
+                (response) => {
+                    setProducers(response.data);
+                },
+            );
+            axios
+            .get("http://localhost:8000/api/admin-companies")
+            .then(
+                (response) => {
+                    setCompanies(response.data);
+                },
+            );
     }, []);
-
+    //метод отвечает за обработку клика выбора фильма
     function handleClick(film) {
         
         getFilmDetail(film);
     };
+    //медот отвечает за добавление фильма
     function handleAddFilm(film) {
-       // console.log(film);
         axios.defaults.withCredentials = true;
         axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
             (response) => {
                 axios.post("http://localhost:8000/api/admin-films", film).then(
                     (response)=>
                 {
-                    //console.log(response);
                     setallfilms(allfilms => [...allfilms, response.data]);
                     getFilmDetail(response.data);
                 },
                 )
             })
     }
+    //метод отвечает за обновление фильма
     function handleUpdateFilm(film) {
-        // console.log(film);
          axios.defaults.withCredentials = true;
          axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
              (response) => {
@@ -65,25 +88,40 @@ export default function Films() {
                         return item.id !== film.id
                     })
                     setallfilms(allfilms => [...array, response.data]);
-                    getFilmDetail(film);
                  },
                  ).then(
                     (response)=>
                     {
-                        axios.post("http://localhost:8000/api/admin-genrefilm",currentFilm.genrefilms).then(
+                        axios.post("http://localhost:8000/api/admin-genrefilm",film.genrefilms).then(
                             (response)=>
                         {
-                            //console.log(response);
-                            getFilmDetail(film);
                         })
                     }, 
-                 )
+                 ).then(
+                    (response)=>
+                    {
+                        axios.post("http://localhost:8000/api/admin-actorfilm",film.actorfilms).then(
+                            (response)=>
+                        {
+                            
+                        })
+                    }, 
+                    ).then(
+                        (response)=>
+                        {
+                            axios.post("http://localhost:8000/api/admin-producerfilm",film.producerfilms).then(
+                                (response)=>
+                            {
+                                getFilmDetail(film);
+                            })
+                        }, 
+                        )
              })
 
             
      }
+     //мотод отвечает за удаление фильма
      function handleDeleteFilm(film) {
-        // console.log(film);
          axios.defaults.withCredentials = true;
          axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
              (response) => {
@@ -99,12 +137,13 @@ export default function Films() {
                  )
              })
      }
-        
+        //рендер страницы
         return (
             <div>
                 <h3>All Films</h3>
                 <ul>
                     {
+                        //вывод списка фильмов
                         allfilms.map((film,i) => {
                             return (
                                 <li onClick={() => handleClick(film)}
@@ -117,7 +156,11 @@ export default function Films() {
                         )
                     }
                 </ul>
-                <Film film={currentFilm} onUpdate={handleUpdateFilm} onDelete={handleDeleteFilm} genres={genres} />
+                {//вывод выбранного фильма
+                }
+                <Film film={currentFilm} onUpdate={handleUpdateFilm} onDelete={handleDeleteFilm} genres={genres} actors={allactors} producers={allproducers} companies={allcompanies}/>
+                {//добавление фильма
+                }
                 <AddFilm onAdd={handleAddFilm} />
                 
             </div>
