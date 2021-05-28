90a698a9ede11cae1878e52f7d2ea5cccec504f2
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-05-28 18:41:01 +0300

    FilmMenu

--- src/components/Admin/Films/Films.js
+++ src/components/Admin/Films/Films.js
@@ -2,6 +2,34 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Film from "components/Admin/Films/Film.js";
 import AddFilm from "components/Admin/Films/AddFilm.js";
+//import menu
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 500,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
 export default function Films() {
     const [allfilms, setallfilms] = useState([]);
     const [currentFilm, setcurrentFilm] = useState(null);
@@ -9,15 +37,22 @@ export default function Films() {
     const [allactors, setActors] = useState();
     const [allproducers, setProducers] = useState();
     const [allcompanies, setCompanies] = useState();
+    const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    function handleChange(e,newValue) {
+        e.preventDefault();
+        setValue(newValue);
+    };
     //метод, который заполняет данными переменную выбранного фильма (перересовка выбраного фильма)
-    function getFilmDetail(film){
+    function getFilmDetail(film) {
         axios
-        .get("http://localhost:8000/api/admin-films/"+film.id)
-        .then(
-            (response) => {
-              setcurrentFilm(response.data);
-            },
-        );
+            .get("http://localhost:8000/api/admin-films/" + film.id)
+            .then(
+                (response) => {
+                    setcurrentFilm(response.data);
+                },
+            );
     }
     //метод, который вызывается первый при загрузки страницы, здесь заполняем фильмами страницу, а также список жанров
     useEffect(() => {
@@ -28,28 +63,28 @@ export default function Films() {
                     setallfilms(response.data);
                 },
             );
-            axios
+        axios
             .get("http://localhost:8000/api/admin-genres")
             .then(
                 (response) => {
                     setGenres(response.data);
                 },
             );
-            axios
+        axios
             .get("http://localhost:8000/api/admin-actors")
             .then(
                 (response) => {
                     setActors(response.data);
                 },
             );
-            axios
+        axios
             .get("http://localhost:8000/api/admin-producers")
             .then(
                 (response) => {
                     setProducers(response.data);
                 },
             );
-            axios
+        axios
             .get("http://localhost:8000/api/admin-companies")
             .then(
                 (response) => {
@@ -59,7 +94,7 @@ export default function Films() {
     }, []);
     //метод отвечает за обработку клика выбора фильма
     function handleClick(film) {
-        
+
         getFilmDetail(film);
     };
     //медот отвечает за добавление фильма
@@ -68,102 +103,118 @@ export default function Films() {
         axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
             (response) => {
                 axios.post("http://localhost:8000/api/admin-films", film).then(
-                    (response)=>
-                {
-                    setallfilms(allfilms => [...allfilms, response.data]);
-                    getFilmDetail(response.data);
-                },
+                    (response) => {
+                        setallfilms(allfilms => [...allfilms, response.data]);
+                        getFilmDetail(response.data);
+                    },
                 )
             })
     }
     //метод отвечает за обновление фильма
     function handleUpdateFilm(film) {
-         axios.defaults.withCredentials = true;
-         axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
-             (response) => {
-                 axios.put("http://localhost:8000/api/admin-films/"+film.id,film).then(
-                     (response)=>
-                 {
-                    var array = allfilms.filter(function(item) {
-                        return item.id !== film.id
-                    })
-                    setallfilms(allfilms => [...array, response.data]);
-                 },
-                 ).then(
-                    (response)=>
-                    {
-                        axios.post("http://localhost:8000/api/admin-genrefilm",film.genrefilms).then(
-                            (response)=>
-                        {
+        axios.defaults.withCredentials = true;
+        axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
+            (response) => {
+                axios.put("http://localhost:8000/api/admin-films/" + film.id, film).then(
+                    (response) => {
+                        var array = allfilms.filter(function (item) {
+                            return item.id !== film.id
                         })
-                    }, 
-                 ).then(
-                    (response)=>
-                    {
-                        axios.post("http://localhost:8000/api/admin-actorfilm",film.actorfilms).then(
-                            (response)=>
-                        {
-                            
-                        })
-                    }, 
-                    ).then(
-                        (response)=>
-                        {
-                            axios.post("http://localhost:8000/api/admin-producerfilm",film.producerfilms).then(
-                                (response)=>
-                            {
+                        setallfilms(allfilms => [...array, response.data]);
+                    },
+                ).then(
+                    (response) => {
+                        axios.post("http://localhost:8000/api/admin-genrefilm", film.genrefilms).then(
+                            (response) => {
+                            })
+                    },
+                ).then(
+                    (response) => {
+                        axios.post("http://localhost:8000/api/admin-actorfilm", film.actorfilms).then(
+                            (response) => {
+
+                            })
+                    },
+                ).then(
+                    (response) => {
+                        axios.post("http://localhost:8000/api/admin-producerfilm", film.producerfilms).then(
+                            (response) => {
                                 getFilmDetail(film);
                             })
-                        }, 
-                        )
-             })
+                    },
+                )
+            })
 
-            
-     }
-     //мотод отвечает за удаление фильма
-     function handleDeleteFilm(film) {
-         axios.defaults.withCredentials = true;
-         axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
-             (response) => {
-                 axios.delete("http://localhost:8000/api/admin-films/"+film.id,film).then(
-                     (response)=>
-                 {
-                    var array = allfilms.filter(function(item) {
-                        return item.id !== currentFilm.id
-                    })
-                    setallfilms(array);
-                    setcurrentFilm(null);
-                 },
-                 )
-             })
-     }
-        //рендер страницы
-        return (
-            <div>
-                <h3>All Films</h3>
-                <ul>
-                    {
-                        //вывод списка фильмов
-                        allfilms.map((film,i) => {
-                            return (
-                                <li onClick={() => handleClick(film)}
-                                    key={i} >
-                                    { film.name}
-                                </li>
-                            );
 
-                        }
-                        )
+    }
+    //мотод отвечает за удаление фильма
+    function handleDeleteFilm(film) {
+        axios.defaults.withCredentials = true;
+        axios.get("http://localhost:8000/" + "sanctum/csrf-cookie").then(
+            (response) => {
+                axios.delete("http://localhost:8000/api/admin-films/" + film.id, film).then(
+                    (response) => {
+                        var array = allfilms.filter(function (item) {
+                            return item.id !== currentFilm.id
+                        })
+                        setallfilms(array);
+                        setcurrentFilm(null);
+                    },
+                )
+            })
+    }
+    //рендер страницы
+    return (
+<div>
+        <Grid container spacing={1}>
+            <Grid xs={2}>
+            <div className={classes.root}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={(e) => handleChange(e)}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+            >
+                {
+                    //вывод списка фильмов
+                    allfilms.map((film, i) => {
+                        return (
+                            <div key={i}>
+                            <Tab onClick={() => handleClick(film)} label={ film.name} {...a11yProps(i)} />
+                            </div>
+                        );
+
                     }
-                </ul>
-                {//вывод выбранного фильма
+                    )
                 }
-                <Film film={currentFilm} onUpdate={handleUpdateFilm} onDelete={handleDeleteFilm} genres={genres} actors={allactors} producers={allproducers} companies={allcompanies}/>
-                {//добавление фильма
-                }
-                <AddFilm onAdd={handleAddFilm} />
-                
+            </Tabs>
             </div>
+            </Grid>
+            <Grid xs={10}>
+            <Film film={currentFilm} onUpdate={handleUpdateFilm} onDelete={handleDeleteFilm} genres={genres} actors={allactors} producers={allproducers} companies={allcompanies} />
+            </Grid>
+        </Grid>
+        
+            
+                   
+                            
+                            
+                     
+                
+            
 
-        );
-    };
+
+            <h3>All Films</h3>
+            
+            {//добавление фильма
+            }
+            <AddFilm onAdd={handleAddFilm} />
+
+        </div>
+
+
+    );
+    
+};
