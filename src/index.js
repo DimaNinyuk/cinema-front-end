4a783f281b1c603c7279db4cf0104ddd633573bb
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-05-30 22:23:08 +0300

    Add Profile User

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 // pages for this product
 import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import UserProfile from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
 import Auth from "views/Auth/Auth.js";
 import LoginGoogle from "components/Auth/LoginGoogle.js"
@@ -21,7 +21,7 @@ ReactDOM.render(
   <Router history={history}>
     <Switch>
       <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/user-profile" component={UserProfile} />
       <Route path="/login-page" component={LoginPage} />
       <Route path="/components" component={Components} />
       <Route path="/auth" component={Auth} />
